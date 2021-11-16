f013264d2a2c21f9e0287940ab46b5a708d2cbc3
felixif <felix.ifrim@gmail.com>
2021-11-16 21:24:27 +0000

    added protected fillable variables

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'text',
+    ];
 
     public function user()
     {
--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,14 +4,10 @@ namespace Database\Factories;
 use App\Models\User;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CommentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
